90cda524bb044af9d0599e24f60b967d0e494db3
Rudy <abcdupuisrudy@gmail.com>
2025-04-06 16:49:57 +0000

    Refactor code structure for improved readability and maintainability

--- pages/index.vue
+++ pages/index.vue
@@ -12,8 +12,26 @@ usePageMeta('index')
     <div class="lg:w-5/12"></div>
     <div class="flex flex-col items-center justify-center gap-14 md:gap-20">
       <BigLogoSvg class="w-10/12 md:w-fit" />
-      <pre class="text-lg md:text-2xl">{{ $t('index.subtitle') }}</pre>
-      <a class="button" href="/">{{ $t('index.cta') }}</a>
+      <pre class="text-lg md:text-2xl">{{ $t('index.hero-banner.subtitle') }}</pre>
+      <a class="button" href="/#find-a-game">{{ $t('index.hero-banner.cta') }}</a>
+    </div>
+  </section>
+  <section id="find-a-game">
+    <h2 class="large-title p-10">{{ $t('index.find-a-game.title') }}</h2>
+    <p class="text-center p-20">Work in progress</p>
+  </section>
+  <section
+    class="w-screen bg-cover bg-center flex items-center justify-center xl:justify-between"
+    style="background-image: url('/images/player-in-forest-background.png')"
+  >
+    <div></div>
+    <div class="px-10 xl:pr-40 py-40 xl:py-60 xl:w-5/12">
+      <h3 class="font-bold text-2xl py-8">{{ $t('index.infos.title1') }}</h3>
+      <p>{{ $t('index.infos.description1') }}</p>
+      <h3 class="font-bold text-2xl py-8">{{ $t('index.infos.title2') }}</h3>
+      <p>{{ $t('index.infos.description2') }}</p>
+      <h3 class="font-bold text-2xl py-8">{{ $t('index.infos.title3') }}</h3>
+      <p>{{ $t('index.infos.description3') }}</p>
     </div>
   </section>
 </template>
--- pages/login.vue
+++ pages/login.vue
@@ -1,9 +1,46 @@
 <script setup lang="ts">
+import { useLocalePath } from '#imports'
 import { usePageMeta } from '~/composables/usePageMeta'
 
 usePageMeta('login')
+const localePath = useLocalePath()
 </script>
 
 <template>
-  <h1>Work in progress</h1>
+  <section
+    class="w-screen pt-40 pb-20 bg-cover bg-center bg-left flex items-center justify-center"
+    style="background-image: url('/images/player-lying-in-the-grass-background.png')"
+  >
+    <div
+      class="bg-background rounded-lg border-primary border-2 border px-40 py-10 flex flex-col items-center justify-center gap-10"
+    >
+      <BigLogoSvg class="w-80" />
+
+      <form class="flex flex-col gap-8 w-full">
+        <div class="flex flex-col gap-2">
+          <label for="email">{{ $t('login.email') }}</label>
+          <input id="email" type="email" :placeholder="$t('login.email')" class="text-input" />
+        </div>
+        <div class="flex flex-col gap-2">
+          <label for="password">{{ $t('login.password') }}</label>
+          <input
+            id="password"
+            type="password"
+            :placeholder="$t('login.password')"
+            class="text-input"
+          />
+        </div>
+
+        <button class="button mt-5">{{ $t('login.submit') }}</button>
+      </form>
+
+      <NuxtLink :to="localePath('/register')" class="button-secondary">
+        {{ $t('login.register') }}
+      </NuxtLink>
+
+      <NuxtLink :to="localePath('/forgot-password')" class="underline hover:text-primary">{{
+        $t('login.forgot-password')
+      }}</NuxtLink>
+    </div>
+  </section>
 </template>
